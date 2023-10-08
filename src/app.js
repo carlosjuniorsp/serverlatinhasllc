77027ffa7a1f942c1fe63ef09a0e98490fd9efde
Carlos <carlosdossantosjuniorsp@gmail.com>
2023-10-07 22:49:27 -0300

    cors install

--- src/app.js
+++ src/app.js
@@ -7,8 +7,10 @@ import {
   deleteData,
 } from "./Controller/Latinhas.js";
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
